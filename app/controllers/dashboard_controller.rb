dac55468cd082cf9faac721653983dfba46fed5a
carlos andres lopez <30788836+05andres@users.noreply.github.com>
2021-12-28 14:49:33 -0500

    production

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    @status= Dashboard.paginate(page: params[:page], per_page: 5)
+    @status= Dashboard.paginate(page: params[:page], per_page: 6)
   end
 end
